74db61ad2892af95bef9bc89d0179d658c1f3968
wangyongst <wangyongst@gmail.com>
2019-08-28 17:35:08 +0800

    create

--- src/main/java/com/spring/boot/sensor/admin/controller/ViewController.java
+++ src/main/java/com/spring/boot/sensor/admin/controller/ViewController.java
@@ -10,17 +10,44 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/view")
 public class ViewController {
 
     @Autowired
     public AdminService adminService;
 
+    @RequestMapping("/caidananniugongnengguanli")
+    public String caidananniugongnengguanli() {
+        return "caidananniugongnengguanli";
+    }
 
-    @RequestMapping("/billdetail-list")
-    public String billdetail(@ModelAttribute ParameterM parameterM, Model model) {
-        model.addAttribute("billid", parameterM.getBillid());
-        model.addAttribute("thymeleafutils", new ThymeleafUtils());
-        return "billdetail-list";
+    @RequestMapping("/quyuguanli")
+    public String quyuguanli() {
+        return "quyuguanli";
+    }
+
+
+    @RequestMapping("/juesequanxian")
+    public String juesequanxian() {
+        return "juesequanxian";
+    }
+
+    @RequestMapping("/xitongbaimingdan")
+    public String xitongbaimingdan() {
+        return "xitongbaimingdan";
+    }
+
+    @RequestMapping("/xitongrizhi")
+    public String xitongrizhi() {
+        return "xitongrizhi";
+    }
+
+    @RequestMapping("/yonghuguanli")
+    public String yonghuguanli() {
+        return "yonghuguanli";
+    }
+
+    @RequestMapping("/zuzhijiagouguanli")
+    public String zuzhijiagouguanli() {
+        return "zuzhijiagouguanli";
     }
 }
