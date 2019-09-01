5bca023a721d950ab692fb64ed47d83ebff7eab0
wangyongst <wangyongst@gmail.com>
2019-09-01 13:13:54 +0800

    home

--- src/main/test/sensor.sql
+++ src/main/test/sensor.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50717
+Source Server Version : 50711
 Source Host           : localhost:3306
 Source Database       : sensor
 
 Target Server Type    : MYSQL
-Target Server Version : 50717
+Target Server Version : 50711
 File Encoding         : 65001
 
-Date: 2019-08-28 15:53:07
+Date: 2019-09-01 13:13:19
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,14 +26,25 @@ CREATE TABLE `dept` (
   `type` varchar(255) DEFAULT NULL,
   `parentid` int(11) DEFAULT NULL,
   `isuse` int(11) DEFAULT NULL,
-  `order` varchar(255) DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
   `remark` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of dept
 -- ----------------------------
+INSERT INTO `dept` VALUES ('1', '11', '111', '1', null, '1', '111', '1111');
+INSERT INTO `dept` VALUES ('2', '', '', '1', null, '1', '1', '');
+INSERT INTO `dept` VALUES ('3', 'sadf sadf as', 'df asdf asdf ', '1', null, '1', '1', 'asdf asdf asdf as ');
+INSERT INTO `dept` VALUES ('4', 'adsf asdf ', 'asdf asdf asdf ', '1', null, '1', '222', 'asdfasd fasdf asf asdf asdf asdf ');
+INSERT INTO `dept` VALUES ('5', '1', '1', '1', null, '1', '1', '12121212');
+INSERT INTO `dept` VALUES ('6', '基石无人机 ', '械a', '2', null, '0', '1', '西南非花木成畦手自栽 asd');
+INSERT INTO `dept` VALUES ('7', '1', '11', '1', null, '1', '1', '11111');
+INSERT INTO `dept` VALUES ('8', '1', '1', '1', null, '1', '1', '1111111');
+INSERT INTO `dept` VALUES ('9', '1', '1', '1', null, '1', '1', '111111');
+INSERT INTO `dept` VALUES ('10', '11', '11', '1', null, '1', '1', '1111111');
+INSERT INTO `dept` VALUES ('11', '撒打发sd', '地花木成畦手自栽 a', '1', null, '1', '1', '槈花木成畦手自栽 花木成畦手自栽 花木成畦手自栽 花木成畦手自栽 ');
 
 -- ----------------------------
 -- Table structure for permission
@@ -43,40 +54,28 @@ CREATE TABLE `permission` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cname` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
+  `urlpath` varchar(255) DEFAULT NULL,
+  `iconpath` varchar(255) DEFAULT NULL,
+  `ischeck` int(11) DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
   `parentid` int(11) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  `updatetime` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of permission
 -- ----------------------------
-INSERT INTO `permission` VALUES ('13', '首页', 'index', '0', '0');
-INSERT INTO `permission` VALUES ('18', '采购管理', 'purch', '0', '0');
-INSERT INTO `permission` VALUES ('24', '财务管理', 'finance', '0', '0');
-INSERT INTO `permission` VALUES ('28', '权限中心', 'permission', '0', '0');
-INSERT INTO `permission` VALUES ('38', '新建', 'index-create', '2', '13');
-INSERT INTO `permission` VALUES ('39', '查询', 'index-query', '2', '13');
-INSERT INTO `permission` VALUES ('40', '修改', 'index-update', '2', '13');
-INSERT INTO `permission` VALUES ('41', '删除', 'index-delete', '2', '13');
-INSERT INTO `permission` VALUES ('42', '导出', 'index-export', '2', '13');
-INSERT INTO `permission` VALUES ('43', '耗材类型', 'index-material', '2', '13');
-INSERT INTO `permission` VALUES ('44', '查询', 'purch-query', '2', '18');
-INSERT INTO `permission` VALUES ('45', '导出', 'purch-export', '2', '18');
-INSERT INTO `permission` VALUES ('46', '新增', 'purch-create', '2', '18');
-INSERT INTO `permission` VALUES ('47', '删除', 'purch-delete', '2', '18');
-INSERT INTO `permission` VALUES ('48', '修改', 'purch-update', '2', '18');
-INSERT INTO `permission` VALUES ('49', '查询', 'finance-query', '2', '24');
-INSERT INTO `permission` VALUES ('50', '导出', 'finance-export', '2', '24');
-INSERT INTO `permission` VALUES ('51', '填写发票号', 'finance-fapiao', '2', '24');
-INSERT INTO `permission` VALUES ('52', '新建', 'permission-create', '2', '28');
-INSERT INTO `permission` VALUES ('53', '查询', 'permission-query', '2', '28');
-INSERT INTO `permission` VALUES ('54', '修改', 'permission-update', '2', '28');
-INSERT INTO `permission` VALUES ('55', '删除', 'permission-delete', '2', '28');
-INSERT INTO `permission` VALUES ('56', '撤回', 'purch-back', '2', '18');
-INSERT INTO `permission` VALUES ('57', '审核', 'finance-yes', '2', '24');
-INSERT INTO `permission` VALUES ('58', '发起', 'purch-start', '2', '18');
-INSERT INTO `permission` VALUES ('59', '价格系数', 'purch-price', '2', '18');
+INSERT INTO `permission` VALUES ('60', '用户管理', 'yonghuguanliP', null, null, null, '1', null, null, null, null);
+INSERT INTO `permission` VALUES ('61', '用户管理', 'yonghuguanli', '/yonghuguanli', null, null, '1', '60', null, null, null);
+INSERT INTO `permission` VALUES ('62', '组织架构管理', 'zuzhijiagouguanli', '/zuzhijiagouguanli', null, null, '1', '60', null, null, null);
+INSERT INTO `permission` VALUES ('63', '角色权限', 'juesequanxian', '/juesequanxian', null, null, '1', '60', null, null, null);
+INSERT INTO `permission` VALUES ('64', '系统管理', 'xitongguanli', '', null, null, '1', null, null, null, null);
+INSERT INTO `permission` VALUES ('65', '菜单按钮功能管理', 'caidananniugongnengguanli', '/caidananniugongnengguanli', null, null, '1', '64', null, null, null);
+INSERT INTO `permission` VALUES ('66', '系统日志', 'xitongrizhi', '/xitongrizhi', null, null, '1', '64', null, null, null);
+INSERT INTO `permission` VALUES ('67', '系统白名单', 'xitongbaimingdan', '/xitongbaimingdan', null, null, '1', '64', null, null, null);
 
 -- ----------------------------
 -- Table structure for role
@@ -85,19 +84,17 @@ DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `projectid` int(11) DEFAULT NULL,
+  `cnname` varchar(255) DEFAULT NULL,
+  `isuse` int(11) DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES ('13', '系统管理员', null);
-INSERT INTO `role` VALUES ('48', '测试角色', '145');
-INSERT INTO `role` VALUES ('49', '普通用户', '145');
-INSERT INTO `role` VALUES ('51', '开发', '175');
-INSERT INTO `role` VALUES ('52', '采购经理', null);
-INSERT INTO `role` VALUES ('53', '业务员', '177');
+INSERT INTO `role` VALUES ('48', '测试角色', null, null, null, null);
 
 -- ----------------------------
 -- Table structure for role2permission
@@ -113,101 +110,14 @@ CREATE TABLE `role2permission` (
 -- ----------------------------
 -- Records of role2permission
 -- ----------------------------
-INSERT INTO `role2permission` VALUES ('266', '13', '13');
-INSERT INTO `role2permission` VALUES ('267', '13', '38');
-INSERT INTO `role2permission` VALUES ('268', '13', '39');
-INSERT INTO `role2permission` VALUES ('269', '13', '40');
-INSERT INTO `role2permission` VALUES ('270', '13', '41');
-INSERT INTO `role2permission` VALUES ('271', '13', '42');
-INSERT INTO `role2permission` VALUES ('272', '13', '43');
-INSERT INTO `role2permission` VALUES ('273', '13', '18');
-INSERT INTO `role2permission` VALUES ('274', '13', '44');
-INSERT INTO `role2permission` VALUES ('275', '13', '45');
-INSERT INTO `role2permission` VALUES ('276', '13', '46');
-INSERT INTO `role2permission` VALUES ('277', '13', '47');
-INSERT INTO `role2permission` VALUES ('278', '13', '48');
-INSERT INTO `role2permission` VALUES ('279', '13', '56');
-INSERT INTO `role2permission` VALUES ('280', '13', '24');
-INSERT INTO `role2permission` VALUES ('281', '13', '49');
-INSERT INTO `role2permission` VALUES ('282', '13', '50');
-INSERT INTO `role2permission` VALUES ('283', '13', '51');
-INSERT INTO `role2permission` VALUES ('284', '13', '57');
-INSERT INTO `role2permission` VALUES ('285', '13', '28');
-INSERT INTO `role2permission` VALUES ('286', '13', '52');
-INSERT INTO `role2permission` VALUES ('287', '13', '53');
-INSERT INTO `role2permission` VALUES ('288', '13', '54');
-INSERT INTO `role2permission` VALUES ('289', '13', '55');
-INSERT INTO `role2permission` VALUES ('330', '13', '51');
-INSERT INTO `role2permission` VALUES ('331', '13', '56');
-INSERT INTO `role2permission` VALUES ('332', '13', '58');
-INSERT INTO `role2permission` VALUES ('333', '13', '59');
-INSERT INTO `role2permission` VALUES ('360', '48', '13');
-INSERT INTO `role2permission` VALUES ('361', '48', '38');
-INSERT INTO `role2permission` VALUES ('362', '48', '39');
-INSERT INTO `role2permission` VALUES ('363', '48', '40');
-INSERT INTO `role2permission` VALUES ('364', '48', '41');
-INSERT INTO `role2permission` VALUES ('365', '48', '42');
-INSERT INTO `role2permission` VALUES ('366', '48', '43');
-INSERT INTO `role2permission` VALUES ('367', '48', '18');
-INSERT INTO `role2permission` VALUES ('368', '48', '44');
-INSERT INTO `role2permission` VALUES ('369', '48', '45');
-INSERT INTO `role2permission` VALUES ('370', '48', '46');
-INSERT INTO `role2permission` VALUES ('371', '48', '47');
-INSERT INTO `role2permission` VALUES ('372', '48', '48');
-INSERT INTO `role2permission` VALUES ('373', '48', '56');
-INSERT INTO `role2permission` VALUES ('374', '48', '58');
-INSERT INTO `role2permission` VALUES ('375', '48', '59');
-INSERT INTO `role2permission` VALUES ('376', '48', '24');
-INSERT INTO `role2permission` VALUES ('377', '48', '49');
-INSERT INTO `role2permission` VALUES ('378', '48', '50');
-INSERT INTO `role2permission` VALUES ('379', '48', '51');
-INSERT INTO `role2permission` VALUES ('380', '48', '57');
-INSERT INTO `role2permission` VALUES ('381', '48', '28');
-INSERT INTO `role2permission` VALUES ('382', '48', '52');
-INSERT INTO `role2permission` VALUES ('383', '48', '53');
-INSERT INTO `role2permission` VALUES ('384', '48', '54');
-INSERT INTO `role2permission` VALUES ('385', '48', '55');
-INSERT INTO `role2permission` VALUES ('388', '49', '38');
-INSERT INTO `role2permission` VALUES ('389', '49', '39');
-INSERT INTO `role2permission` VALUES ('390', '49', '44');
-INSERT INTO `role2permission` VALUES ('391', '49', '49');
-INSERT INTO `role2permission` VALUES ('392', '49', '53');
-INSERT INTO `role2permission` VALUES ('408', '51', '13');
-INSERT INTO `role2permission` VALUES ('409', '51', '38');
-INSERT INTO `role2permission` VALUES ('410', '51', '18');
-INSERT INTO `role2permission` VALUES ('411', '51', '44');
-INSERT INTO `role2permission` VALUES ('412', '51', '24');
-INSERT INTO `role2permission` VALUES ('413', '51', '49');
-INSERT INTO `role2permission` VALUES ('414', '51', '28');
-INSERT INTO `role2permission` VALUES ('415', '51', '52');
-INSERT INTO `role2permission` VALUES ('452', '52', '13');
-INSERT INTO `role2permission` VALUES ('453', '52', '38');
-INSERT INTO `role2permission` VALUES ('454', '52', '39');
-INSERT INTO `role2permission` VALUES ('455', '52', '40');
-INSERT INTO `role2permission` VALUES ('456', '52', '41');
-INSERT INTO `role2permission` VALUES ('457', '52', '42');
-INSERT INTO `role2permission` VALUES ('458', '52', '43');
-INSERT INTO `role2permission` VALUES ('459', '52', '18');
-INSERT INTO `role2permission` VALUES ('460', '52', '44');
-INSERT INTO `role2permission` VALUES ('461', '52', '45');
-INSERT INTO `role2permission` VALUES ('462', '52', '46');
-INSERT INTO `role2permission` VALUES ('463', '52', '47');
-INSERT INTO `role2permission` VALUES ('464', '52', '48');
-INSERT INTO `role2permission` VALUES ('465', '52', '56');
-INSERT INTO `role2permission` VALUES ('466', '52', '58');
-INSERT INTO `role2permission` VALUES ('467', '52', '59');
-INSERT INTO `role2permission` VALUES ('468', '52', '24');
-INSERT INTO `role2permission` VALUES ('469', '52', '49');
-INSERT INTO `role2permission` VALUES ('470', '52', '50');
-INSERT INTO `role2permission` VALUES ('471', '52', '51');
-INSERT INTO `role2permission` VALUES ('472', '52', '57');
-INSERT INTO `role2permission` VALUES ('473', '53', '44');
-INSERT INTO `role2permission` VALUES ('474', '53', '45');
-INSERT INTO `role2permission` VALUES ('475', '53', '46');
-INSERT INTO `role2permission` VALUES ('476', '53', '47');
-INSERT INTO `role2permission` VALUES ('477', '53', '48');
-INSERT INTO `role2permission` VALUES ('478', '53', '56');
-INSERT INTO `role2permission` VALUES ('479', '53', '58');
+INSERT INTO `role2permission` VALUES ('266', '48', '60');
+INSERT INTO `role2permission` VALUES ('267', '48', '61');
+INSERT INTO `role2permission` VALUES ('268', '48', '62');
+INSERT INTO `role2permission` VALUES ('269', '48', '63');
+INSERT INTO `role2permission` VALUES ('270', '48', '64');
+INSERT INTO `role2permission` VALUES ('271', '48', '65');
+INSERT INTO `role2permission` VALUES ('272', '48', '66');
+INSERT INTO `role2permission` VALUES ('273', '48', '67');
 
 -- ----------------------------
 -- Table structure for user
@@ -222,13 +132,33 @@ CREATE TABLE `user` (
   `mobile` varchar(255) DEFAULT NULL,
   `telephone` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `islogin` int(11) DEFAULT NULL,
-  `roleid` int(32) DEFAULT NULL,
+  `roleid` int(11) DEFAULT NULL,
   `remark` varchar(255) DEFAULT NULL,
-  `islock` int(11) DEFAULT NULL,
+  `deptid` int(11) DEFAULT NULL,
+  `isuse` int(11) DEFAULT NULL,
+  `finger` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('85', '111', '111', '21232f297a57a5a743894a0e4a801fc3', null, null, null, null, null, null, null, '0', null);
+INSERT INTO `user` VALUES ('86', '11', '1', '1', null, null, null, null, null, null, null, null, null);
+INSERT INTO `user` VALUES ('87', null, null, null, null, null, null, null, null, null, null, null, null);
+
+-- ----------------------------
+-- Table structure for user2role
+-- ----------------------------
+DROP TABLE IF EXISTS `user2role`;
+CREATE TABLE `user2role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) DEFAULT NULL,
+  `roleid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=481 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user2role
+-- ----------------------------
+INSERT INTO `user2role` VALUES ('480', '85', '48');
