ec90b28cd500c4b500836d7864072df1b0645cd5
wangyongst <wangyongst@gmail.com>
2019-09-04 12:08:44 +0800

    create

--- src/main/resources/static/myjs/wy/zuzhijiagouguanli-modal.js
+++ src/main/resources/static/myjs/wy/zuzhijiagouguanli-modal.js
@@ -1,4 +1,29 @@
 $(function () {
+
+    var setting2 = {
+        check: {
+            enable: true,
+            chkStyle: "radio",
+            radioType: "all"
+        },
+        view: {
+            dblClickExpand: false
+        },
+        data: {
+            simpleData: {
+                enable: true
+            }
+        },
+
+    };
+
+    $.get("/admin/dept/list",
+        function (result) {
+            if (result.status) {
+                $.fn.zTree.init($("#treeDemo1"), setting2, result.data);
+            }
+        });
+
     $("#deptSave").click(function () {
         $.post("/admin/dept/sud?" + $('#deptForm').serialize(),
             function (result) {
