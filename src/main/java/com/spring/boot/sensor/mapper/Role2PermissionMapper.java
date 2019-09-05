4b58f76e99b301a130fa99c830caa10e7adf5b51
wangyongst <wangyongst@gmail.com>
2019-09-05 17:39:22 +0800

    create

--- src/main/java/com/spring/boot/sensor/mapper/Role2PermissionMapper.java
+++ src/main/java/com/spring/boot/sensor/mapper/Role2PermissionMapper.java
@@ -19,7 +19,7 @@ public interface Role2PermissionMapper {
     @Delete("delete from role2permission where roleid = #{roleid}")
     void deleteByRoleid(@Param("roleid") int roleid);
 
-    @Select("insert into role2rermission (roleid, permissionid) values(#{roleid}, #{permissionid})")
+    @Select("insert into role2permission (roleid, permissionid) values(#{roleid}, #{permissionid})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insertRole2Permission(Role2Permission role2Permission);
 }
