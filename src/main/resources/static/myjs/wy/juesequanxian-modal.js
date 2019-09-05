35dba9e8957f94cd0b8cd6b8b932a06565d5992e
wangyongst <wangyongst@gmail.com>
2019-09-05 15:48:53 +0800

    create

--- src/main/resources/static/myjs/wy/juesequanxian-modal.js
+++ src/main/resources/static/myjs/wy/juesequanxian-modal.js
@@ -1,4 +1,29 @@
 $(function () {
+
+    var setting = {
+        view: {
+            selectedMulti: false
+        },
+        check: {
+            enable: true
+        },
+        data: {
+            simpleData: {
+                enable: true
+            }
+        },
+        edit: {
+            enable: false
+        }
+    };
+
+    $.get("/admin/permission/list?isuse=0",
+        function (result) {
+            if (result.status) {
+                $.fn.zTree.init($("#treeDemo2"), setting, result.data);
+            }
+        });
+
     $("#roleSave").click(function () {
         $.post("/admin/role/sud?" + $('#roleForm').serialize(),
             function (result) {
